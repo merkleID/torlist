e10b86a30ccd2ddce0b17b777ffbc91ae3aeb095
merkleID <luca@merkle.id>
2022-08-03 12:23:06 +0200

    updates

--- update-ipset.sh
+++ update-ipset.sh
@@ -8,9 +8,9 @@ ipt=$(which iptables)
 (
 curl -o $HOME/torlist.txt https://raw.githubusercontent.com/merkleID/torlist/main/torlist.txt
 
-$ips flush tor
-$ipt -D INPUT -m set --match-set tor src -j DROP
-while read IP; do $ips -q -A tor $IP; done < $HOME/torlist.txt
-$ipt -A INPUT -m set --match-set tor src -j DROP
+/sbin/ipset flush tor
+/usr/sbin/iptables -D INPUT -m set --match-set tor src -j DROP
+while read IP; do /sbin/ipset -q -A tor $IP; done < $HOME/torlist.txt
+/usr/sbin/iptables -A INPUT -m set --match-set tor src -j DROP
 echo $(date)
 ) 2>&1 | tee -a $HOME/torlist-update.log
