fa260d23a76a005dff0fb6d1956584d51e15792d
merkleID <luca@merkle.id>
2022-08-04 18:17:45 +0200

    changes

--- update-ipset.sh
+++ update-ipset.sh
@@ -2,13 +2,13 @@
 
 #change PATHS according to your setup
 
-ips=$(which ipset)
-ipt=$(which iptables)
+#ips=$(which ipset)
+#ipt=$(which iptables)
 
 (
 curl -o $HOME/torlist.txt https://raw.githubusercontent.com/merkleID/torlist/main/torlist.txt
 
-/sbin/ipset flush tor
+/usr/sbin/ipset flush tor
 /usr/sbin/iptables -D INPUT -m set --match-set tor src -j DROP
 while read IP; do /sbin/ipset -q -A tor $IP; done < $HOME/torlist.txt
 /usr/sbin/iptables -A INPUT -m set --match-set tor src -j DROP
