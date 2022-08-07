e4028e4d27de3239d48889f5d9237ed5cd1f1fcc
merkleID <luca@merkle.id>
2022-08-07 02:27:44 +0200

    changes

--- run-first.sh
+++ run-first.sh
@@ -6,3 +6,8 @@ ipt=$(which iptables)
 $ips create tor iphash
 $ipt -A INPUT -m set --match-set tor src -j DROP
 
+apt install netfilter-persistent ipset-persistent iptables-persistent
+netfilter-persistent save
+systemctl enable netfilter-persistent
+systemctl start netfilter-persistent
+systemctl status netfilter-persistent
